84926110cd5660729cc35c5d59ec8893631fdd0b
Delnegend <kiennguyen19323@gmail.com>
2024-09-26 18:52:38 +0700

    feat: add names to configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import hagemanto from "./lib/index.js";
 
 export default [
-	{ files: ["test.ts"] },
-	{ ignores: ["eslint.config.js", "lib/index.js", "lib/rules.js"] },
+	{ name: "hagemanto/test/include", files: ["test.ts"] },
+	{ name: "hagemanto/test/exclude", ignores: ["eslint.config.js", "lib/index.js", "lib/rules.js"] },
 
-	...hagemanto(),
-	{ languageOptions: { parserOptions: { project: "./tsconfig.json" } } },
+	...hagemanto({enablePrettier: false}),
+	{ name: "hagemanto/test/parser-options", languageOptions: { parserOptions: { project: "./tsconfig.json" } } },
 ];
