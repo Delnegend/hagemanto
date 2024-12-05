396fbd11f2b2eea014161c8eb9aae967f48e74b2
Delnegend <kiennguyen19323@gmail.com>
2024-12-05 11:00:13 +0700

    feat: update example config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,6 @@ export default [
 	{ name: "hagemanto/test/include", files: ["test.ts"] },
 	{ name: "hagemanto/test/exclude", ignores: ["eslint.config.js", "lib/index.js", "lib/rules.js"] },
 
-	...hagemanto({enablePrettier: false}),
+	...hagemanto({styler: "stylistic", enableJsx: true, enableTs: true}),
 	{ name: "hagemanto/test/parser-options", languageOptions: { parserOptions: { project: "./tsconfig.json" } } },
 ];
