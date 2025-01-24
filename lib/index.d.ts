2f1db234e6d6001d6a68039a39f033ad9586a59f
Delnegend <kiennguyen19323@gmail.com>
2025-01-24 09:51:06 +0700

    refactor: separated d.ts file

---
+++ lib/index.d.ts
@@ -0,0 +1,63 @@
+export interface HagemantoOptions {
+    /**
+     * Use prettier or stylistic for code styling
+     *
+     * Default: undefined
+     */
+    styler?: "prettier" | "stylistic"
+
+    /**
+     * Enable JSX support
+     *
+     * Default: true
+     */
+    enableJsx?: boolean
+
+    /**
+     * Enable Vue support
+     *
+     * Default: false
+     */
+    enableVue?: boolean
+
+    /**
+     * Enable typescript support
+     *
+     * Default: true
+     */
+    enableTs?: boolean
+
+    /**
+     * Enable tailwind support
+     *
+     * Default: true
+     */
+    enableTailwind?: boolean
+
+    /**
+     * Enable sorting imports
+     *
+     * Default: true
+     */
+    sortImports?: boolean
+
+    /**
+     * Enable default languageOptions config from tseslint
+     *
+     * https://typescript-eslint.io/getting-started/typed-linting/
+     *
+     * Default: true
+     */
+    enableDefaultLanguageOptions?: boolean
+
+    /**
+     * The `extraFileExtensions` in `laguageOptions.parserOptions`
+     *
+     * Default: []
+     */
+    extraFileExtensions?: string[]
+}
+
+declare function hagemanto(options?: HagemantoOptions): Array<any>
+
+export default hagemanto
